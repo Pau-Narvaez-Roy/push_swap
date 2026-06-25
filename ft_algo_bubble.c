95e66eadd2c7f257f83e72b7f73f987b659ef23d
Ptitchat <pau.roy.narvaez@gmail.com>
2026-06-25 09:55:54 +0200

    algo

---
+++ ft_algo_bubble.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_algo_bubble.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alcristo <alcristo@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/25 09:49:36 by alcristo          #+#    #+#             */
+/*   Updated: 2026/06/25 09:53:10 by alcristo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "includes/push_swap.h"
+
+void	ft_algo_bubble(t_list *a)
+{
+	double	entropy;
+
+	entropy = ft_entropy(a);
+	while (entropy < 1)
+}
