d09a3bdb27c15a769cafc454e3276cbfbb71e84f
Ptitchat <pau.roy.narvaez@gmail.com>
2026-06-25 14:05:59 +0200

    .h

--- srcs/includes/push_swap.h
+++ srcs/includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: pnarvaez <pnarvaez@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/23 14:22:19 by pnarvaez          #+#    #+#             */
-/*   Updated: 2026/06/24 16:49:47 by alcristo         ###   ########.fr       */
+/*   Updated: 2026/06/25 11:29:48 by pnarvaez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,19 +23,20 @@ typedef struct s_list
 	struct s_list	*next;
 }					t_list;
 
-double	ft_entropy(t_list *a);
-void	ft_swap(t_list *lst);
-void	ft_push(t_list *to, t_list *from);
-void	ft_rotate(t_list **lst);
-void	ft_rrotate(t_list **lst);
-void	ft_multiple(t_list *a, t_list *b, int mov);
-t_list	*ft_create_stack(char **argv);
-void	ft_algo_fewnums(t_list *a);
-void	ft_algo_simple(t_list *a);
-int		ft_lstsize(t_list *lst);
-t_list	*ft_lstnew(int content);
-t_list	*ft_lstlast(t_list *lst);
-void	ft_lstadd_back(t_list **lst, t_list *new);
-void	ft_lstadd_front(t_list **lst, t_list *new);
+double			ft_entropy(t_list *a);
+void			ft_swap(t_list *lst);
+void			ft_push(t_list *to, t_list *from);
+void			ft_rotate(t_list **lst);
+void			ft_rrotate(t_list **lst);
+void			ft_multiple(t_list *a, t_list *b, int mov);
+t_list			*ft_create_stack(char **argv);
+void			ft_algo_fewnums(t_list *a);
+void			ft_algo_simple(t_list *a);
+int				ft_lstsize(t_list *lst);
+t_list			*ft_lstnew(int content);
+t_list			*ft_lstlast(t_list *lst);
+void			ft_lstadd_back(t_list **lst, t_list *new);
+void			ft_lstadd_front(t_list **lst, t_list *new);
+unsigned int	*ft_standar(t_list	*stack);
 
 #endif
